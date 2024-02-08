0704bfeaf347c5d509af23b087e8dd53adc21b0e
Sadhana U <01fe22bca237@kletech.ac.in>
2024-02-08 11:41:05 +0530

    simple calclulator operation

--- calclulator.java
+++ calclulator.java
@@ -11,6 +11,12 @@ public class calclulator {
     float division(float operand_1,float operand_2){
         return(operand_1 / operand_2);
     }
+    float square(float operand_1){
+        return(operand_1* operand_1);
+    }
+    float cube(float operand_1){
+        return((operand_1* operand_1)*operand_1);
+    }
     public static void main(String[] args){
         float operand_1=10;
         float operand_2=5;
@@ -23,6 +29,11 @@ public class calclulator {
         System.out.println("Result of multiplication is: "+mul_result);
         float div_result=cal.division(operand_1,operand_2);
         System.out.println("Result of division is: "+div_result);
+        float sqr_result=cal.square(operand_1);
+        System.out.println("Result of division is: "+sqr_result);
+        float cube_result=cal.cube(operand_1);
+        System.out.println("Result of division is: "+cube_result);
+
 
     }
 }
