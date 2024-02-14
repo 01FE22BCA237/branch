aab0c0dfbbbe47d4e2fa6c35f2223a2c45230464
sadhana <01fe22bca237@kletecch.ac.in>
2024-02-14 15:47:01 +0530

    adding two file for extended calci branch

---
+++ extendedcalculator.java
@@ -0,0 +1,19 @@
+public class extendedcalculator{
+    public int areaoftri() {
+        int base=5;
+        int hight=8;
+        int area=(1/2)*base*hight;
+        return area;
+    }
+    public int areaofsquare(){
+        int side=5;
+        int area_of_side=side*side;
+        return area_of_side;
+    }
+    public int modulus(){
+        int a=5;
+        int b=3;
+        int mod=a%b;
+        return mod;
+    }
+}
